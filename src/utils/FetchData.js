fa3d21dbd26ab820a136a8ca1110aad58129a49d
abdullah al hilfi <abdullahalhilfi21@gmail.com>
2022-11-03 14:30:54 -0500

    Created H. scroll for everybody part

--- src/utils/FetchData.js
+++ src/utils/FetchData.js
@@ -2,8 +2,7 @@ export const exerciseOptions = {
   method: "GET",
   // url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
   headers: {
-    // "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-    "X-RapidAPI-Key": "4dfc059f59mshfbcc80152a0965ap1aa4d3jsndf5761350e07",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
